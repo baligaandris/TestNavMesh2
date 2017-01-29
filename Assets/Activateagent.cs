1dfc4a1b8c1925baf333d01f813f228957df95a7
baligaandris <baligaandris@gmail.com>
2017-01-29 13:40:34 +0000

    Created

---
+++ Assets/Activateagent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Activateagent : MonoBehaviour {
+
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<NavMeshAgent>().SetDestination(GameObject.Find("Target").transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
